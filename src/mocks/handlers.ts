e603a9e3c5a89e95d19fb334da12f2bcaa8caca0
Andrei Filonenko <afilonenko.ie@gmail.com>
2021-08-06 05:50:51 +0300

    Remove unnecessarry files

--- src/mocks/handlers.ts
+++
@@ -1,8 +0,0 @@
-import { rest } from 'msw';
-import { beer } from "../test-utils/fixtures";
-
-export const handlers = [
-    rest.get('https://api.punkapi.com/v2/beers/random', (
-        req, res, ctx
-    ) => res(ctx.status(200), ctx.json(beer)))
-]
